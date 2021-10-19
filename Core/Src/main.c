b0c35c155957680ff77676083888a87fa911ec42
Ekrem Köse <ekrem.kose@outlook.com>
2021-10-19 19:01:43 +0300

    farukcum deneme yaptim seviliyorsun <3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -77,7 +77,7 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
+  // deneme commit
   /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
